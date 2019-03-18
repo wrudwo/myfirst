5f423811bc9364d9ac2f29e7e4191c37054c1385
wrudwo <wrudwo@naver.com>
2019-03-18 21:55:09 +0900

    node js is my life

---
+++ src/main.js
@@ -0,0 +1,18 @@
+var http = require('http')
+var fs = require('fs')
+
+function onRequest(req, res){
+    res.writeHead(200,{'Content-Type':'text/html'})
+    fs.readFile('src/templates/index.html', function(error, data){
+        if(error){
+            res.writeHead(404)
+            res.write('file not found')
+        } else{
+            res.write(data)
+        }
+        res.end()
+    })
+
+}
+
+http.createServer(onRequest).listen(8080);
