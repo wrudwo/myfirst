a3ed70819246cf8503cecf5a4ba09c2ba0032f59
wrudwo <wrudwo@naver.com>
2019-03-20 17:24:06 +0900

    express practice1

--- src/practice/1/main.js
+++ src/practice/1/main.js
@@ -1,10 +1,23 @@
 var express = require('express')
+var path = require('path')
 var app = express()
 
-app.get('/', function(req,res){
-    res.send('Hello World')
+app.get('/dd',function(req,res){
+    res.sendFile(path.join(__dirname + '/index.html'))
 })
 
 app.listen(3000,function(){
     console.log('Example app listening on port 3000!')
 })
+
+/*
+app.get('/2', function(req,res){
+    res.send('gsads')
+})
+app.get('/3', function(req,res){
+    res.send('sdjklajflka')
+})
+app.get('/4', function(req,res){
+    res.sendFile (path.join(__dirname + '/index.html'))
+})
+*/
